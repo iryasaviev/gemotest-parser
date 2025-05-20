126eb945d98255eb92a1b375af1d2cd43304cf4a
iryasaviev <ir.yasaviev@yandex.ru>
2025-05-20 20:05:55 +0300

    Refactoring.

--- parsers.js
+++ parsers.js
@@ -1,39 +1,40 @@
 async function parse() {
-	let table = document.getElementsByClassName("listtable result-table")[0];
-	let tableRows = table.getElementsByTagName("tr");
+    let table = document.getElementsByClassName("listtable result-table")[0],
+        tableRows = table.getElementsByTagName("tr");
 
-	let orders = [];
-	let num = 0;
-	for(let row of tableRows) {
-	num++;
-    if(num <= 2) continue;
+    let orders = [],
+        num = 0;
+    for (let row of tableRows) {
+        num++;
+        if (num <= 2) continue;
+
         try {
-            let order = row.getElementsByTagName("td")[3];
-    	    let pacientFullName = row.getElementsByTagName("td")[7];
+            let order = row.getElementsByTagName("td")[3],
+                pacientFullName = row.getElementsByTagName("td")[7];
             orders[orders.length - 1] = order.innerText;
 
-    	    console.log(pacientFullName.innerText);
+            console.log(pacientFullName.innerText);
 
-            let url = "/OrderPrint_Pdf.php?OrderIDDetail=" + order.innerText;
-        	let response = await fetch(url);
-        	
-        	if(response.ok) {
-        		let blob = await response.blob();
-                let pdfUrl = URL.createObjectURL(blob);
+            let url = "/OrderPrint_Pdf.php?OrderIDDetail=" + order.innerText,
+                response = await fetch(url);
 
-                let a = document.createElement("a");
+            if (response.ok) {
+                let blob = await response.blob(),
+                    pdfUrl = URL.createObjectURL(blob),
+                    a = document.createElement("a");
+
                 a.href = pdfUrl;
-                a.download = `${order.innerText} ${pacientFullName.innerText.replaceAll(",", "").replaceAll(" ", "-")}.pdf`;
+                a.download = `${order.innerText} ${pacientFullName.innerText
+                    .replaceAll(",", "")
+                    .replaceAll(" ", "-")}.pdf`;
                 document.body.appendChild(a);
                 a.click();
-        	}
-        	else {
-        		console.log(response.status);
-        	}
+            } else {
+                console.log(response.status);
+            }
+        } catch (err) {
+            console.error(err);
         }
-        catch {
-            continue;
-        }
-	}
+    }
 }
 await parse();
